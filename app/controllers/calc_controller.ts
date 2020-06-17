b7bbfafaa1e7c3065ca93f7c3ece56923565f3af
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2020-06-17 20:01:30 +0700

    Unit test for controller and view

--- app/controllers/calc_controller.ts
+++ app/controllers/calc_controller.ts
@@ -2,6 +2,12 @@ import * as Express from 'express'
 import * as CalcView from '../views/calc_view'
 import * as CalcModel from '../models/calc_model'
 
+export type CalcRequest = {
+  operator: CalcModel.Operator;
+  firstNumber: number;
+  secondNumber: number;
+}
+
 export function Calc(req: Express.Request, res: Express.Response<CalcView.CalcResult>): Express.Response<CalcView.CalcResult> {
   const { operator, secondNumber, firstNumber } = req.body
   if (!Object.values(CalcModel.Operator).includes(operator)) {
