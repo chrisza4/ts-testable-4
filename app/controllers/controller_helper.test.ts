715c672e1a289684f3343191d680810cb7950957
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2020-06-19 23:11:42 +0700

    proper test

--- app/controllers/controller_helper.test.ts
+++ app/controllers/controller_helper.test.ts
@@ -34,6 +34,40 @@ describe('createExpressHandler', () => {
     expect(response.body).toEqual({ message: 'Hello Chris' })
   })
 
+  it('can inject querystring', async () => {
+    const controller = async (body: TestRequest, querystring: ControllerHelper.QueryString): Promise<TestResponse> => {
+      return {
+        message: `Hello ${querystring.greetUser}`
+      }
+    }
+
+    const handler = ControllerHelper.createExpressHandler(controller)
+    const app = Express()
+    app.use(Express.json())
+    app.get('/', handler)
+    const response = await Supertest(app).get('/?greetUser=Chris')
+    expect(response.body).toEqual({ message: 'Hello Chris' })
+  })
+
+  it('can inject params', async () => {
+    const controller = async (
+      body: TestRequest,
+      querystring: ControllerHelper.QueryString,
+      params: ControllerHelper.Params
+    ): Promise<TestResponse> => {
+      return {
+        message: `Hello ${params.user}`
+      }
+    }
+
+    const handler = ControllerHelper.createExpressHandler(controller)
+    const app = Express()
+    app.use(Express.json())
+    app.get('/:user', handler)
+    const response = await Supertest(app).get('/Chris')
+    expect(response.body).toEqual({ message: 'Hello Chris' })
+  })
+
   it('Change validation error to 422', async () => {
     const controller = async (body: TestRequest): Promise<TestResponse> => {
       throw new ControllerHelper.ValidationError('Invalid input')
