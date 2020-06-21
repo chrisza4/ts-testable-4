73b2db02318853a2f27ad6ff0b8db4c107a20330
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2020-06-21 10:07:14 +0700

    Cleanup a lot

--- app/controllers/calc_controller.ts
+++ app/controllers/calc_controller.ts
@@ -2,35 +2,22 @@ import * as Express from 'express'
 import * as CalcView from '../views/calc_view'
 import * as CalcModel from '../models/calc_model'
 
-export type CalcRequest = {
-  operator: CalcModel.Operator;
-  firstNumber: number;
-  secondNumber: number;
+type HttpErrorResponse = {
+  status: number;
+  errorMessage: string;
 }
+type CalcResponse = CalcView.CalcResult | HttpErrorResponse
 
-export function Calc(req: Express.Request, res: Express.Response<CalcView.CalcResult>): Express.Response<CalcView.CalcResult> {
+export function Calc(req: Express.Request, res: Express.Response<CalcResponse>): Express.Response<CalcView.CalcResult> {
   const { operator, secondNumber, firstNumber } = req.body
+  // Check operator
   if (!Object.values(CalcModel.Operator).includes(operator)) {
     return res.status(422).send({
-      success: false,
+      status: 422,
       errorMessage: 'Invalid operator'
     })
   }
 
   const result = CalcModel.calculate({ firstNumber, secondNumber, operator })
   return res.json(CalcView.calcResultView(result))
-}
-
-// What we want
-export function CalcController(requestBody: any): CalcView.CalcResult {
-  const { operator, secondNumber, firstNumber } = requestBody
-  if (!Object.values(CalcModel.Operator).includes(operator)) {
-    return {
-      success: false,
-      errorMessage: 'Invalid operator'
-    }
-  }
-
-  const result = CalcModel.calculate({ firstNumber, secondNumber, operator })
-  return CalcView.calcResultView(result)
 }
--- app/views/calc_view.ts
+++ app/views/calc_view.ts
@@ -1,18 +1,9 @@
-type CalcSuccessResult = {
-  success: true;
+export type CalcResult = {
   result: number;
 }
 
-type ErrorResult = {
-  success: false;
-  errorMessage: string;
-}
-
-export type CalcResult = ErrorResult | CalcSuccessResult
-
 export function calcResultView(result: number): CalcResult {
   return {
-    success: true,
     result
   }
 }
